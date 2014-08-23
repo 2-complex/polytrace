b3d2496af040df1e975b1b059bcfeef30c0b4a08
Peterson Trethewey <peterson.trethewey@gmail.com>
2014-08-23 14:54:29 -0700

    moved Polygon class into its own file

--- main.js
+++ main.js
@@ -15,59 +15,6 @@ var CELLWIDTH = 100;
 var CELLHEIGHT = 100;
 
 
-function Polygon()
-{
-    this.vertices = [];
-    this.closed = false;
-}
-
-Polygon.prototype.draw = function()
-{
-    for ( var i=0; i<this.vertices.length; i++ )
-    {
-        var x = this.vertices[i][0];
-        var y = this.vertices[i][1];
-        var w = 4;
-        var h = 4;
-        CTX.fillStyle = polygonStrokeColor;
-        CTX.strokeStyle = polygonStrokeColor;
-        CTX.strokeRect(x-w, y-w, 2*w, 2*h);
-    }
-
-    if( this.vertices.length > 0 )
-    {
-        CTX.beginPath();
-
-        CTX.lineWidth = 2;
-        CTX.strokeStyle = polygonStrokeColor;
-
-        var x = this.vertices[0][0];
-        var y = this.vertices[0][1];
-        CTX.moveTo(x, y);
-
-        for ( var i=1; i<this.vertices.length; i++ )
-        {
-            var x = this.vertices[i][0];
-            var y = this.vertices[i][1];
-            CTX.lineTo(x, y);
-        }
-
-        if( this.closed )
-        {
-            var x = this.vertices[0][0];
-            var y = this.vertices[0][1];
-            CTX.lineTo(x, y);
-        }
-
-        CTX.stroke();
-    }
-}
-
-Polygon.prototype.close = function()
-{
-    this.closed = true;
-}
-
 
 // Colors of things
 var gridColor = "hsla(0, 0%, 50%, 0.5)";
